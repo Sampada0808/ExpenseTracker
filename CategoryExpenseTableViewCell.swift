0444bd39d0d5acc0e10c005a9696295d92eea967
Sampada0808 <sampadashankar88@gmail.com>
2025-04-05 23:11:38 +0530

    Updated the CategoryExpenseTableViewCell

--- CategoryExpenseTableViewCell.swift
+++ CategoryExpenseTableViewCell.swift
@@ -8,16 +8,25 @@
 import UIKit
 
 class CategoryExpenseTableViewCell: UITableViewCell {
+    static let identifier = "ExpenseTable"
+
+    @IBOutlet weak var backgroundIconView: UIView!
+    @IBOutlet weak var categoryIcon: UIImageView!
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var categoryName: UILabel!
+    
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        backgroundIconView.layer.cornerRadius = backgroundIconView.frame.height / 2
+        backgroundIconView.clipsToBounds = true
+        backgroundIconView.backgroundColor = UIColor(named: "Medicine")?.withAlphaComponent(0.5)
+        backgroundIconView.contentMode = .scaleAspectFit
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
--- ExpenseTracker/HomeViewController.swift
+++ ExpenseTracker/HomeViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
 
+    @IBOutlet weak var categoryTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        categoryTableView.dataSource = self
+        categoryTableView.estimatedRowHeight = 72
+        categoryTableView.rowHeight = UITableView.automaticDimension
     }
+    
+    
 
 
+}
+
+extension HomeViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CategoryExpenseTableViewCell.identifier, for: indexPath)
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
 }
 
