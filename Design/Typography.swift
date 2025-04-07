36796775e6b50b1c9eba2e572d2137b78c33dcd8
Sampada0808 <sampadashankar88@gmail.com>
2025-04-07 20:47:34 +0530

    Add global font and color constants for app styling

---
+++ Design/Typography.swift
@@ -0,0 +1,8 @@
+//
+//  Typography.swift
+//  ExpenseTracker
+//
+//  Created by Sampada Shankar on 07/04/25.
+//
+
+import Foundation
---
+++ ExpenseTracker/CategoryExpenseDataModel.swift
@@ -0,0 +1,8 @@
+//
+//  CategoryExpenseDataModel.swift
+//  ExpenseTracker
+//
+//  Created by Sampada Shankar on 07/04/25.
+//
+
+import Foundation
